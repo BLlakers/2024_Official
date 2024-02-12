5dd884765cc9ed1c34a179c3034cbc8c7cf61946
BLlakers <jdonnelly@boyslatinmd.com>
2024-02-12 16:05:36 -0500

    second

--- src/main/java/frc/robot/subsystems/JsonING.java
+++ src/main/java/frc/robot/subsystems/JsonING.java
@@ -1,11 +1,20 @@
 package frc.robot.subsystems;
+//JsonING becuse thats what it is
 
-import java.io.FileReader; 
-import java.util.Iterator; 
-import java.util.Map; 
-  
-import org.json.simple.JSONArray; 
-import org.json.simple.JSONObject; 
-import org.json.simple.parser.*;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
-//public class JsonING extends 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+
+public class JsonING{
+    public static void main(String[] args) throws IOException, ParseException {
+        URL url = new URL("http://example.com/json");
+        JSONParser parser = new JSONParser();
+        JSONObject json = (JSONObject) parser.parse(new InputStreamReader(url.openStream()));
+        System.out.println(json);
+    }
+}
