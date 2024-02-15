2d2caa4383c71024218472a21395f82618c67cfb
BLlakers <jdonnelly@boyslatinmd.com>
2024-02-14 19:18:50 -0500

    WIP maybe working Intake control

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
@@ -18,86 +19,94 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 
-public class Intake extends SubsystemBase  {
-    
+public class Intake extends SubsystemBase {
+
     public CANSparkMax intakeAngleMtr = new CANSparkMax(Constants.intakeAngleMtrC,
-      com.revrobotics.CANSparkLowLevel.MotorType.kBrushless);
+            com.revrobotics.CANSparkLowLevel.MotorType.kBrushless);
     public CANSparkMax intakeWheelMtr1 = new CANSparkMax(Constants.intakeWheelMtr1C,
-      com.revrobotics.CANSparkLowLevel.MotorType.kBrushless);
+            com.revrobotics.CANSparkLowLevel.MotorType.kBrushless);
     public CANSparkMax intakeWheelMtr2 = new CANSparkMax(Constants.intakeWheelMtr2C,
-      com.revrobotics.CANSparkLowLevel.MotorType.kBrushless);
+            com.revrobotics.CANSparkLowLevel.MotorType.kBrushless);
+    private static final double intakeInteriorAngleRadians = Units.degreesToRadians(30);
     public RelativeEncoder intakeAngleMtrEnc = intakeAngleMtr.getEncoder();
-    public RelativeEncoder intakeWheelMtr1Enc = intakeWheelMtr1.getEncoder(); 
-    public RelativeEncoder intakeWheelMtr2Enc =  intakeWheelMtr2.getEncoder();
-    public double d3 = .1527;
-    public double IntakePosConversion = (d3/Math.PI) /100; 
-  public ArmFeedforward IntakeFeedForward = new ArmFeedforward(0, 0, 0);
+    public RelativeEncoder intakeWheelMtr1Enc = intakeWheelMtr1.getEncoder();
+    public RelativeEncoder intakeWheelMtr2Enc = intakeWheelMtr2.getEncoder();
+
+    public ArmFeedforward IntakeFeedForward = new ArmFeedforward(0, 0, 0);
     public PIDController m_ArmController = new PIDController(0, 0, 0);
-    public Intake(){
+
+    public Intake() {
         // intakeWheelMtr1.follow(intakeWheelMtr2);
+        double intakeAngleMotorPositionConversion = 2 * Math.PI; // revolutions -> radians
+        double intakeAngleMotorVelocityConversion = intakeAngleMotorPositionConversion / 60; // rpm -> radians/second
+        intakeAngleMtrEnc.setPositionConversionFactor(intakeAngleMotorPositionConversion);
+        intakeAngleMtrEnc.setVelocityConversionFactor(intakeAngleMotorVelocityConversion);
+
+        intakeWheelMtr2.follow(intakeWheelMtr1, true); // pair the intake wheels
     }
 
     @Override
-
     public void periodic() {
         // armRotationMtr1.follow(armRotationMtr2);
 
-        SmartDashboard.putNumber("Intake Position", intakeAngleMtrEnc.getPosition());
+        SmartDashboard.putNumber("Robot/Intake/Position", getIntakeAngle().getDegrees());
+        SmartDashboard.putNumber("Robot/Intake/Encoder/Position", getEncoderAngle().getDegrees());
     }
 
     public Command RaiseIntake() {
         return run(
-            () -> {
-            intakeAngleMtr.set(.5);  
-            });
-      }
+                () -> {
+                    intakeAngleMtr.set(.5);
+                });
+    }
 
     public Command LowerIntake() {
         return runOnce(
-            () -> {
-            intakeAngleMtr.set(-.5);
-            });
-      }
+                () -> {
+                    intakeAngleMtr.set(-.5);
+                });
+    }
 
-public Command StopIntake(){
-    return runOnce(
-     () -> {intakeAngleMtr.set(0);
-    });
-}
+    public Command StopIntake() {
+        return runOnce(
+                () -> {
+                    intakeAngleMtr.set(0);
+                });
+    }
+
     public Command RunIntakeWheels() {
         return runOnce(
-            () -> {
-        intakeWheelMtr1.set(.5);
-        intakeWheelMtr2.set(-.5);
-            });
-      }
+                () -> {
+                    intakeWheelMtr1.set(.5);
+                });
+    }
 
     public Command StopIntakeWheels() {
         return runOnce(
-            () -> {
-        intakeWheelMtr1.set(0);
-        intakeWheelMtr2.set(0);
-            });
-      }
-      public Command ReverseIntakeWheels() {
-        return runOnce(
-            () -> {
-        intakeWheelMtr1.set(1);
-        intakeWheelMtr2.set(-1);
-            });
-      }
-
-public void setIntakeAngle(Rotation2d angle){
-    double IntakeAngleFeedforward = IntakeFeedForward.calculate(angle.getRadians(), 0);
-    double IntakeAngleFeedback = m_ArmController.calculate(getIntakeAngle().getRadians(), angle.getRadians());
-    intakeAngleMtr.setVoltage(IntakeAngleFeedback + IntakeAngleFeedforward); //TODO INCORRECT
-}
-
-    public Rotation2d getIntakeAngle() {
-        // TODO
-        return new Rotation2d();
+                () -> {
+                    intakeWheelMtr1.set(0);
+                });
     }
 
+    public Command ReverseIntakeWheels() {
+        return runOnce(
+                () -> {
+                    intakeWheelMtr1.set(1);
+                });
+    }
 
+    public void setIntakeAngle(Rotation2d angle) {
+        double IntakeAngleFeedforward = IntakeFeedForward.calculate(angle.getRadians(), 0);
+        double IntakeAngleFeedback = m_ArmController.calculate(getIntakeAngle().getRadians(), angle.getRadians());
+        intakeAngleMtr.setVoltage(IntakeAngleFeedback + IntakeAngleFeedforward); // TODO INCORRECT
+    }
+
+    public Rotation2d getEncoderAngle() {
+        return Rotation2d.fromRadians(intakeAngleMtrEnc.getPosition());
+    }
+
+    public Rotation2d getIntakeAngle() {
+        return getEncoderAngle().plus(Rotation2d.fromRadians(intakeInteriorAngleRadians));
+    }
 
 }
