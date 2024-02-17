22a7c53c9fddc9761775dcb81d197879a44bf47c
Liam Griesacker <93160178+LIAM-G-344@users.noreply.github.com>
2024-02-17 15:30:03 -0500

    ir

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,8 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.SwerveModule;
+import com.revrobotics.ColorSensorV3;
+import edu.wpi.first.wpilibj.I2C;
 
 //some imports no longer needed but leaving them here untill final version
 
@@ -17,6 +19,9 @@ public class Robot extends TimedRobot {
   private Arm m_Arm;
   String codeVersion = "0.0";
   
+  //color sensor, it communicates over the only i2c port onboard
+  private final I2C.Port i2c =I2C.Port.kOnboard;
+  private final ColorSensorV3 m_ColorSensor = new ColorSensorV3(i2c);
   // commit
   @Override
   public void robotInit() {
@@ -35,6 +40,10 @@ public class Robot extends TimedRobot {
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
+
+    //dose as it sounds, prints proximity
+    int proximity = m_ColorSensor.getProximity();
+    System.out.println(proximity);
   }
 
   @Override
