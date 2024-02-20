8d4ce07e394d43d99b6d470d50c5b6e5e4b234c1
Dimitri Lezcano <dlezcan1@jhu.edu>
2024-02-20 01:41:34 -0500

    Cleaned up command scheduling
    Signed-off-by: Dimitri Lezcano <dlezcan1@jhu.edu>

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.PIDCommand;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
@@ -64,6 +65,8 @@ public class Shooter extends SubsystemBase {
             m_limitSwitchTop = new DigitalInput(Constants.shooterLimitSwitchTopDIO);
         if (Constants.shooterLimitSwitchBottomDIO >= 0)
             m_limitSwitchBottom = new DigitalInput(Constants.shooterLimitSwitchBottomDIO);
+
+        CalibrateShooterAngle().schedule(); // schedule to calibrate the shooter angle when able
     }
 
     @Override
