4179e7dcf28299b1eebd434b4c632e5ff404b5fe
Dimitri Lezcano <dlezcan1@jhu.edu>
2024-02-20 00:10:26 -0500

    Updated shooter and orientation code to be more functional

--- src/main/java/frc/robot/commands/OrientShooterAngle.java
+++ src/main/java/frc/robot/commands/OrientShooterAngle.java
@@ -12,35 +12,43 @@ import edu.wpi.first.wpilibj2.command.ProfiledPIDCommand;
 import frc.robot.subsystems.Shooter;
 
 public class OrientShooterAngle extends ProfiledPIDCommand {
-int jared = 1;
-    public OrientShooterAngle(Shooter shooter, Rotation2d goal, int jared)
-    {   
+    private Shooter m_Shooter;
+
+    private static double s_kP = 0.5;
+    private static double s_kI = 0.0;
+    private static double s_kD = 0.0;
+    
+    private static final double s_maxShooterAnglingVelocity     = Shooter.s_angleMotorSpeedPercentage; // motor percentage power
+    private static final double s_maxShooterAnglingAcceleration = s_maxShooterAnglingVelocity / 2;     // motor percentage power
+
+    public OrientShooterAngle(Shooter shooter, Rotation2d goal) {
         super(
-            new ProfiledPIDController(0.5, 0, 0, new TrapezoidProfile.Constraints(0.05, 1.0)),
-            () -> { return shooter.GetShooterAngle().getRadians(); },
-            goal.getRadians(),
-            (output, setpoint) -> shooter.SetShooterAngleSpeed(output),
-            shooter
-        );
+                new ProfiledPIDController(s_kP, s_kI, s_kD, new TrapezoidProfile.Constraints(s_maxShooterAnglingVelocity, s_maxShooterAnglingAcceleration)),
+                () -> {
+                    return shooter.GetShooterAngle().getRadians();
+                },
+                goal.getRadians(),
+                (output, setpoint) -> shooter.SetShooterAngleSpeedPercentage(output),
+                shooter);
+                
+        getController().enableContinuousInput(-Math.PI, Math.PI);
+        m_Shooter = shooter;
     }
 
     @Override
-    public void initialize()
-    {
+    public void initialize() {
         super.initialize();
-        getController().setTolerance(Units.degreesToRadians(0.5));
+        getController().setTolerance(Units.degreesToRadians(0.5)); // set 0.5 degree tolerance for error
     }
 
     @Override
-    public void end(boolean interrupted)
-    {
-        // TODO
+    public void end(boolean interrupted) {
+        m_Shooter.AngleStop();
     }
 
     @Override
-    public boolean isFinished()
-    {
+    public boolean isFinished() {
         return getController().atGoal();
     }
-    
+
 }
