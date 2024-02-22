60806ac538076f216a1baecd66e256fa94cb2329
Dimitri Lezcano <dlezcan1@jhu.edu>
2024-02-21 22:40:00 -0500

    Removed unused imports for AprilAlignCommand
    Signed-off-by: Dimitri Lezcano <dlezcan1@jhu.edu>

--- src/main/java/frc/robot/commands/AprilAlignCommand.java
+++ src/main/java/frc/robot/commands/AprilAlignCommand.java
@@ -10,15 +10,11 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
-import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants;
-import frc.robot.Robot;
 import frc.robot.subsystems.DriveTrainPID;
 
 // TODO DO 1 PID AT A TIME !!!!!
