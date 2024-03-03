308c733cf6e9c7ced698090b3e4edea2a2304c1a
Dimitri Lezcano <dlezcan1@jhu.edu>
2024-03-03 17:48:50 -0500

    Updated SwerveDriveCommand for speed handling cleanup & deadband application
    Signed-off-by: Dimitri Lezcano <dlezcan1@jhu.edu>

--- src/main/java/frc/robot/commands/SwerveDriveCommand.java
+++ src/main/java/frc/robot/commands/SwerveDriveCommand.java
@@ -1,7 +1,9 @@
 package frc.robot.commands;
+
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
@@ -18,7 +20,11 @@ public class SwerveDriveCommand extends Command {
 
   private BooleanSupplier m_RunHalfSpeed;
 
-  public SwerveDriveCommand(DoubleSupplier _leftY, DoubleSupplier _leftX, DoubleSupplier _rightX, DoubleSupplier _AccelerateRT,
+  private static final double kDriveMaxSpeed = 0.8 * DriveTrain.kMaxSpeed;
+  private static final double kTurnMaxSpeed = 0.8 * DriveTrain.kMaxTurnAngularSpeed;
+
+  public SwerveDriveCommand(DoubleSupplier _leftY, DoubleSupplier _leftX, DoubleSupplier _rightX,
+      DoubleSupplier _AccelerateRT,
       DriveTrain _dTrain) {
     m_leftY = _leftY;
     m_leftX = _leftX;
@@ -29,7 +35,8 @@ public class SwerveDriveCommand extends Command {
     addRequirements(m_DriveTrain);
   }
 
-  public SwerveDriveCommand(DoubleSupplier _leftY, DoubleSupplier _leftX, DoubleSupplier _rightX, DoubleSupplier _AccelerateRT,
+  public SwerveDriveCommand(DoubleSupplier _leftY, DoubleSupplier _leftX, DoubleSupplier _rightX,
+      DoubleSupplier _AccelerateRT,
       DriveTrain _dTrain, BooleanSupplier _halfSpeedCondition) {
     m_leftY = _leftY;
     m_leftX = _leftX;
@@ -43,12 +50,7 @@ public class SwerveDriveCommand extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    /*
-     * m_DriveTrain.brDrive.setIdleMode(CANSparkMax.IdleMode.kCoast);
-     * m_DriveTrain.frDrive.setIdleMode(CANSparkMax.IdleMode.kCoast);
-     * m_DriveTrain.blDrive.setIdleMode(CANSparkMax.IdleMode.kCoast);
-     * m_DriveTrain.flDrive.setIdleMode(CANSparkMax.IdleMode.kCoast);
-     */
+
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -60,50 +62,39 @@ public class SwerveDriveCommand extends Command {
     double leftY = m_leftY.getAsDouble();
     double rightX = m_rightX.getAsDouble();
 
-    // Finds the X Value of the Left Stick on the Controller and Takes Care of Joystick Drift
-    if (Math.abs(leftX) < Constants.Controller.deadzone) {
-      x = 0.0;
-    } else {
-      x = -leftX;
-    }
+    // Finds the X Value of the Left Stick on the Controller and Takes Care of
+    // Joystick Drift
+    x = MathUtil.applyDeadband(-leftX, Constants.Controller.deadzone);
 
-    // Finds the Y Value of the Left Stick on the Controller and Takes Care of Joystick Drift
-    if (Math.abs(leftY) < Constants.Controller.deadzone) {
-      y = 0.0;
-    } else {
-      y = -leftY;
-    }
+    // Finds the Y Value of the Left Stick on the Controller and Takes Care of
+    // Joystick Drift
+    y = MathUtil.applyDeadband(-leftY, Constants.Controller.deadzone);
 
-    // Finds the X Value of the Right Stick on the Controller and Takes Care of Joystick Drift
-    if (Math.abs(rightX) < Constants.Controller.deadzone) {
-      rot = 0.0;
-    } else {
-      rot = -Math.signum(rightX) * (Math.abs(rightX) - Constants.Controller.deadzone) / (1 - Constants.Controller.deadzone); // this calculation proportinonalizes our controller based off of our deadzone
-    }
+    // Finds the X Value of the Right Stick on the Controller and Takes Care of
+    // Joystick Drift
+    rot = MathUtil.applyDeadband(-rightX, Constants.Controller.deadzone);
+
     RT = AccelerateRT;
 
     double normalizingFactor = Math.hypot(x, y);
-    if (normalizingFactor > 0)
-    {
+    if (normalizingFactor > 0) {
       x /= normalizingFactor;
       y /= normalizingFactor;
     }
-    double xSpeed = y * DriveTrain.kMaxSpeed * RT;
-    double ySpeed = x * DriveTrain.kMaxSpeed * RT;
-    double rotSpeed = rot * DriveTrain.kMaxTurnAngularSpeed;
+    double xSpeed = y * SwerveDriveCommand.kDriveMaxSpeed * RT;
+    double ySpeed = x * SwerveDriveCommand.kDriveMaxSpeed * RT;
+    double rotSpeed = rot * SwerveDriveCommand.kTurnMaxSpeed;
 
-    if (m_RunHalfSpeed.getAsBoolean() == true){
+    if (m_RunHalfSpeed.getAsBoolean() == true) {
       xSpeed /= 2;
       ySpeed /= 2;
       rotSpeed /= 2;
     }
 
-    SmartDashboard.putNumber("Robot/Controller/Command/X Speed", xSpeed);
-    SmartDashboard.putNumber("Robot/Controller/Command/Y Speed", ySpeed);
+    SmartDashboard.putNumber("DriveTrain/Controller/Command/X Speed", xSpeed);
+    SmartDashboard.putNumber("DriveTrain/Controller/Command/Y Speed", ySpeed);
 
-    m_DriveTrain.drive(xSpeed*.8, ySpeed*.8, rotSpeed*.8);
-    //Pose2d pose = m_DriveTrain.getPose2d(); UNUSED. Would print pose2d out
-    //System.out.println(pose);
+    m_DriveTrain.drive(xSpeed, ySpeed, rotSpeed);
   }
 
   // Called once the command ends or is interrupted.
