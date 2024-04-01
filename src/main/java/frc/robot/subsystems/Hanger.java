994ef95be95a71b651084d80335f282f0cf4491f
BLlakers <jdonnelly@boyslatinmd.com>
2024-03-31 20:00:17 -0400

    Added Hanger module

--- src/main/java/frc/robot/subsystems/Hanger.java
+++ src/main/java/frc/robot/subsystems/Hanger.java
@@ -1,60 +1,50 @@
 package frc.robot.subsystems;
 
-import frc.robot.Constants;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.util.sendable.SendableBuilder;
-import edu.wpi.first.wpilibj.DigitalInput;
+import com.revrobotics.CANSparkLowLevel;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Hanger extends SubsystemBase {
-  private DigitalInput hangLeftMagSwitch = new DigitalInput(Constants.Port.hangerLeftMagSwitchDIOC);
-  private DigitalInput hangRightMagSwitch =
-      new DigitalInput(Constants.Port.hangerRightMagSwitchDIOC);
-  private CANSparkMax hangerLeftMtr =
-      new CANSparkMax(
-          Constants.Hanger.LeftMtrC, com.revrobotics.CANSparkLowLevel.MotorType.kBrushless);
-  private CANSparkMax hangerRightMtr =
-      new CANSparkMax(
-          Constants.Hanger.RightMtrC, com.revrobotics.CANSparkLowLevel.MotorType.kBrushless);
-
-  private RelativeEncoder hangerLeftMtrEnc = hangerLeftMtr.getEncoder();
-  private RelativeEncoder hangerRightMtrEnc = hangerRightMtr.getEncoder();
-
+  private HangerModule leftHanger;
+  private HangerModule rightHanger;
+  
   double hangSpeedUp = 0.75;
   double hangSpeedDown = -0.75;
 
   public Hanger() { // Limelight - get Rotation3d rel to tag. / get navx pose (Can navx get 3d?)
     setName("Hanger");
-    ResetHangEnc();
+    leftHanger = new HangerModule(Constants.Hanger.LeftMtrC, CANSparkLowLevel.MotorType.kBrushless, Constants.Port.hangerLeftMagSwitchDIOC);
+    rightHanger = new HangerModule(Constants.Hanger.RightMtrC, CANSparkLowLevel.MotorType.kBrushless, Constants.Port.hangerRightMagSwitchDIOC);
+  
   }
 
-  public void ResetHangEnc() {
-    hangerLeftMtrEnc.setPosition(0.0);
-    hangerRightMtrEnc.setPosition(0.0);
-  }
+  
 
   public Command ResetHangCmd() {
     return this.runOnce(this::ResetHangEnc);
   }
 
   public void LeftHangUp() {
-    hangerLeftMtr.set(hangSpeedUp);
+    leftHanger.MoveHang(hangSpeedUp);
   }
 
   public void LeftHangDown() {
-    hangerLeftMtr.set(hangSpeedDown);
+    leftHanger.MoveHang(hangSpeedDown);
   }
 
   public void LeftHangStop() {
-    hangerLeftMtr.set(0);
+    leftHanger.MoveHang(0);
   }
 
   public void HangStop() {
-    hangerLeftMtr.set(0);
-    hangerRightMtr.set(0);
+    leftHanger.MoveHang(0);
+    rightHanger.MoveHang(0);
   }
 
   public Command HangStopCommand() {
@@ -62,32 +52,36 @@ public class Hanger extends SubsystemBase {
   }
 
   public void RightHangUp() {
-    hangerRightMtr.set(hangSpeedUp);
+    rightHanger.MoveHang(hangSpeedUp);
   }
 
   public void RightHangDown() {
-    hangerRightMtr.set(hangSpeedDown);
+    rightHanger.MoveHang(hangSpeedDown);
   }
 
   public void RightHangStop() {
-    hangerRightMtr.set(0);
+    rightHanger.MoveHang(0);
   }
 
   public boolean LeftHangIsDown() {
-    return !hangLeftMagSwitch.get();
+    return leftHanger.HangIsDown();
   }
 
   public boolean RightHangIsDown() {
-    return !hangRightMagSwitch.get();
+    return rightHanger.HangIsDown();
   }
 
   public double GetLeftPosition() {
-    return hangerLeftMtrEnc.getPosition();
+    return leftHanger.GetPosition();
   }
 
   public double GetRightPosition() {
-    return hangerRightMtrEnc.getPosition();
+    return rightHanger.GetPosition();
   }
+  public void ResetHangEnc (){
+  leftHanger.ResetHangEnc(); 
+  rightHanger.ResetHangEnc();
+}
 
   /** Raises hang when Held. Will stop at top position */
   public Command RaiseHangAuto() {
@@ -128,12 +122,12 @@ public class Hanger extends SubsystemBase {
   public void initSendable(SendableBuilder builder) {
     super.initSendable(builder);
 
-    builder.addDoubleProperty("Left/Hang Pos", hangerLeftMtrEnc::getPosition, null);
-    builder.addDoubleProperty("Right/Hang Pos", hangerRightMtrEnc::getPosition, null);
-    builder.addDoubleProperty("Left/Current", hangerLeftMtr::getOutputCurrent, null);
-    builder.addDoubleProperty("Right/Current", hangerRightMtr::getOutputCurrent, null);
-    builder.addDoubleProperty("Left/Hang Velo", hangerLeftMtrEnc::getVelocity, null);
-    builder.addDoubleProperty("Right/Hang Velo", hangerRightMtrEnc::getVelocity, null);
+    builder.addDoubleProperty("Left/Hang Pos", leftHanger.hangerMtrEnc::getPosition, null);
+    builder.addDoubleProperty("Right/Hang Pos", rightHanger.hangerMtrEnc::getPosition, null);
+    builder.addDoubleProperty("Left/Current", leftHanger.hangerMtr::getOutputCurrent, null);
+    builder.addDoubleProperty("Right/Current", rightHanger.hangerMtr::getOutputCurrent, null);
+    builder.addDoubleProperty("Left/Hang Velo", leftHanger.hangerMtrEnc::getVelocity, null);
+    builder.addDoubleProperty("Right/Hang Velo", rightHanger.hangerMtrEnc::getVelocity, null);
     builder.addBooleanProperty("Left/MagSwitch Engaged", this::LeftHangIsDown, null);
     builder.addBooleanProperty("Right/MagSwitch Engaged", this::RightHangIsDown, null);
   }
---
+++ src/main/java/frc/robot/subsystems/HangerModule.java
@@ -0,0 +1,63 @@
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.DigitalInput;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+
+public class HangerModule extends SubsystemBase {
+  public DigitalInput hangerMagSwitch;
+  public CANSparkMax hangerMtr;
+  public RelativeEncoder hangerMtrEnc;
+
+
+  public HangerModule(int hangMtrPort,com.revrobotics. CANSparkLowLevel.MotorType MotorType, int hangLimitSwitchPort) {
+    hangerMtr = new CANSparkMax(hangMtrPort, MotorType);
+    hangerMtrEnc = hangerMtr.getEncoder();
+    hangerMagSwitch = new DigitalInput(hangLimitSwitchPort);
+    
+    ResetHangEnc();
+  }
+
+  public void ResetHangEnc() {
+    hangerMtrEnc.setPosition(0.0);
+    }
+
+  public Command ResetHangCmd() {
+    return this.runOnce(this::ResetHangEnc);
+  }
+
+  public void MoveHang(double speed){
+    hangerMtr.set(speed); //
+  }
+
+  public void HangStop() {
+    hangerMtr.set(0);
+  }
+
+  public Command HangStopCommand() {
+    return this.runOnce(this::HangStop);
+  }
+  public boolean HangIsDown() {
+    return !hangerMagSwitch.get();
+  }
+
+  public double GetPosition() {
+    return hangerMtrEnc.getPosition();
+  }
+
+  /** Raises hang when Held. Will stop at top position */
+  
+
+
+  @Override
+  public void initSendable(SendableBuilder builder) {
+    super.initSendable(builder);
+
+    builder.addDoubleProperty("Hang Pos", hangerMtrEnc::getPosition, null);
+    builder.addDoubleProperty("Current", hangerMtr::getOutputCurrent, null);
+    builder.addDoubleProperty("Left/Hang Velo", hangerMtrEnc::getVelocity, null);
+    builder.addBooleanProperty("Left/MagSwitch Engaged", this::HangIsDown, null);
+  }
+}
