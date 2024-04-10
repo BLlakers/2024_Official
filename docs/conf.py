0ee587c847113e0a84d189bf2ecb499984d177f4
BLlakers <jdonnelly@boyslatinmd.com>
2024-04-09 21:42:07 -0400

    TRY LAST

--- docs/conf.py
+++ docs/conf.py
@@ -17,15 +17,10 @@ templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and directories to ignore when looking for source files.
 exclude_patterns = []
-
-extensions = ["myst_parser"]
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.
 html_theme = 'sphinx_rtd_theme'
-
-# Output file base name for HTML help builder.
-htmlhelp_basename = 'YourFRCJavaProjectNameDoc'
 
 # -- Path configuration ------------------------------------------------------
 
