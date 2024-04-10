7159f25e0385dabb7c276d772c63b7b39c2279be
BLlakers <jdonnelly@boyslatinmd.com>
2024-04-09 21:29:47 -0400

    Lets Try again

---
+++ conf.py
@@ -0,0 +1,33 @@
+# Configuration file for Sphinx documentation
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
+# -- Project information -----------------------------------------------------
+
+project = 'Your FRC Java Project Name'
+author = 'Your Name'
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here
+extensions = []
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and directories to ignore when looking for source files.
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.
+html_theme = 'sphinx_rtd_theme'
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = 'YourFRCJavaProjectNameDoc'
+
+# -- Path configuration ------------------------------------------------------
+
+# If your documentation needs a minimal Sphinx version, state it here.
+# needs_sphinx = '1.0'
