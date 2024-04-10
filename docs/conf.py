a44f1fed5c8e44396fa3385d63a37bc59daad00b
BLlakers <jdonnelly@boyslatinmd.com>
2024-04-09 21:47:00 -0400

    I wanna give up so i will.

--- docs/conf.py
+++ docs/conf.py
@@ -5,22 +5,27 @@ sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'Offical_2024'
-author = 'Jared Forchheimer'
+project = 'Your FRC Java Project Name'
+author = 'Your Name'
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here
+#extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+#templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and directories to ignore when looking for source files.
-exclude_patterns = []
+#exclude_patterns = []
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.
 #html_theme = 'sphinx_rtd_theme'
+
+# Output file base name for HTML help builder.
+#htmlhelp_basename = 'YourFRCJavaProjectNameDoc'
 
 # -- Path configuration ------------------------------------------------------
 
